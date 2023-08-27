0358101a8498a4b7e89a795ce6865f2863ec6684
audwo <kmjstj3@naver.com>
2023-08-27 23:59:19 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
+import LoginModal from "@/components/modals/LoginModal";
+import useLoginModal from "@/hooks/useLoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import useRegisterModal from "@/hooks/useRegisterMdoal";
+import { Session } from "inspector";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Toaster/>
+      {registerModal.isOpen && <RegisterModal />}
+      {loginModal.isOpen && <LoginModal />}
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </SessionProvider>
   );
 }
